279996a957b24279b7a5b3c9352c2a02cb94c159
bloshko <mis.bloko@gmail.com>
2025-01-25 19:13:47 +0100

    I'm walking here

--- scenes/player.gd
+++ scenes/player.gd
@@ -55,9 +55,13 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		if not $WalkingAudio.playing:
+			$WalkingAudio.play()
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED) 
+		if $WalkingAudio.playing:
+			$WalkingAudio.stop()
 	
 	# All of the other processing functions go here
 	_process_interact()
