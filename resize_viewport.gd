3f54426abf36e96d40f6b2659bc3178fcc250c40
Adam Krupicka <adam@breachvr.com>
2025-01-25 23:42:38 +0100

    Viewport scaling implement

--- resize_viewport.gd
+++ resize_viewport.gd
@@ -5,11 +5,11 @@ extends SubViewportContainer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	get_tree().get_root().size_changed.connect(_on_resized) 
+	_on_resized()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
 
 func _on_resized() -> void:
 	size = get_viewport().size
