acac450f4e2d7b5c207e005c5efa027f2f4ae401
Adam Krupicka <adam@breachvr.com>
2025-01-26 11:15:55 +0100

    Add all the notes and letters

--- scenes/notes/note.gd
+++ scenes/notes/note.gd
@@ -1,16 +1,18 @@
 extends Interactible3D
 
+@export_multiline var text: String
+
 @onready var note_ui = $Note
+@onready var note_ui_label = $Note/RichTextLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
+	note_ui_label.text = text
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
 
 func _on_interact() -> void:
 	Engine.time_scale = 0
